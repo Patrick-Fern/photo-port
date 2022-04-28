ac7ec89afa3a5c2fc4b725942c5af13dba6293ce
patfern <patrick.j.fernbach@gmail.com>
2022-04-27 23:03:37 -0400

    tests

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 
-
 function App() {
   const [categories] = useState([
     {
@@ -25,10 +24,9 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
-
     </div>
   );
 }
