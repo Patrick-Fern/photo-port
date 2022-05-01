a242c36d5b1a4abeecec26b8f8756d7693732278
patfern <patrick.j.fernbach@gmail.com>
2022-05-01 19:16:54 -0400

    conditionally rendered modal

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ function App() {
         setContactSelected={setContactSelected}
       ></Nav>
       <main>
-       {!contactSelected ? (
-         <>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-         </>
-       ): (
-         <ContactForm></ContactForm>
-       )} 
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
